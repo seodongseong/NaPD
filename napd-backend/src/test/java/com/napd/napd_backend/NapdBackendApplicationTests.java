69c927efa8338a96af5ea03e62dfe4de22ed0881
seodongseong <ehdtjd0821@naver.com>
2025-12-15 20:28:13 +0900

    test: 기본 테스트 파일 추가

---
+++ napd-backend/src/test/java/com/napd/napd_backend/NapdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.napd.napd_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NapdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
